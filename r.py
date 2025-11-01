2e21a2efdd55b6e06b1d11f2571c4d7a027a9eba
amjad-228 <amjadalsabry28@gmail.com>
2025-11-01 05:37:33 +0300

    eeee

--- r.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-# Ensure parent directory (backend/) is on sys.path so we can import app.py
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-
-from app import app  # FastAPI instance
-
-
